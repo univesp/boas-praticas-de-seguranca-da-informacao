874a2e766b33247e09161c4420eed068cdc37f1a
Kauê Delfino <128550129+KDelfino@users.noreply.github.com>
2025-09-18 13:40:26 -0300

    Add files via upload

---
+++ javascripts/events.js
@@ -0,0 +1,340 @@
+$(document).ready(function(){
+
+  var click1 = 0;
+  var click2 = 0;
+  var click3 = 0;
+  var click4 = 0;
+  var click5 = 0;
+  var click6 = 0;
+  var click7 = 0;
+  var placar = 0;
+
+
+  $(".botao").click(function(){
+        $('img').css("filter", "grayscale(0%)");
+        $(".botao").hide();
+  $(".marcador-1").click(function(){
+  $(".lateral h2, .lateral p").hide();
+  $(".botao, .botao2").hide();
+  $('.item_imagem1').show();
+  $(".mensagem1").show();
+  $(".mensagem2").hide();
+  $(".mensagem3").hide();
+  $(".mensagem4").hide();
+  $(".mensagem5").hide();
+  $(".mensagem6").hide();
+  $(".mensagem7").hide();
+  click1++;
+  if(click1>0 && click2>0  && click3>0 && click4>0  && click5>0 && click6>0 && click7>0){
+  $(".botao2").show();
+  $(".botao2").click(function(){
+  $(".lateral h2, .lateral p").show();
+  $(".botao2").hide();
+  $(".mensagem2").hide();
+  $(".mensagem1").hide();
+  $(".mensagem3").hide();
+  $(".mensagem4").hide();
+  $(".mensagem5").hide();
+  $(".mensagem6").hide();
+  $(".mensagem7").hide();
+  $(".item_imagem1").hide();
+  $(".item_imagem2").hide();
+  $(".item_imagem3").hide();
+  $(".item_imagem4").hide();
+  $(".item_imagem5").hide();
+  $(".item_imagem6").hide();
+  $(".item_imagem7").hide();
+   click1 = 0;
+   click2 = 0;
+   click3 = 0;
+   click4 = 0;
+   click5 = 0;
+   click6 = 0;
+   click7 = 0;
+   placar = 0;
+});
+      }
+      if(click1==1){
+        placar++;
+    $(".final").find("p").text(placar + '/7');
+      }
+});
+  $(".marcador-2").click(function(){
+  $(".lateral h2, .lateral p").hide();
+  $(".botao, .botao2").hide();
+  $('.item_imagem2').show();
+  $(".mensagem2").show();
+  $(".mensagem1").hide();
+  $(".mensagem3").hide();
+  $(".mensagem4").hide();
+  $(".mensagem5").hide();
+  $(".mensagem6").hide();
+  $(".mensagem7").hide();
+  click2++;
+  if(click1>0 && click2>0  && click3>0 && click4>0  && click5>0 && click6>0 && click7>0){
+        $(".botao2").show();
+        $(".botao2").click(function(){
+  $(".lateral h2, .lateral p").show();
+  $(".botao2").hide();
+  $(".mensagem2").hide();
+  $(".mensagem1").hide();
+  $(".mensagem3").hide();
+  $(".mensagem4").hide();
+  $(".mensagem5").hide();
+  $(".mensagem6").hide();
+  $(".mensagem7").hide();
+  $(".item_imagem1").hide();
+  $(".item_imagem2").hide();
+  $(".item_imagem3").hide();
+  $(".item_imagem4").hide();
+  $(".item_imagem5").hide();
+  $(".item_imagem6").hide();
+  $(".item_imagem7").hide();
+   click1 = 0;
+   click2 = 0;
+   click3 = 0;
+   click4 = 0;
+   click5 = 0;
+   click6 = 0;
+   click7 = 0;
+   placar = 0;
+});
+      }
+      if(click2==1){
+        placar++;
+    $(".final").find("p").text(placar + '/7');
+      }
+});
+  $(".marcador-3").click(function(){
+    $(".lateral h2, .lateral p").hide();
+  $(".botao, .botao2").hide();
+  $('.item_imagem3').show();
+  $(".mensagem3").show();
+  $(".mensagem2").hide();
+  $(".mensagem1").hide();
+  $(".mensagem4").hide();
+  $(".mensagem5").hide();
+  $(".mensagem6").hide();
+  $(".mensagem7").hide();
+  click3++;
+  if(click1>0 && click2>0  && click3>0 && click4>0  && click5>0 && click6>0 && click7>0){
+        $(".botao2").show();
+        $(".botao2").click(function(){
+  $(".lateral h2, .lateral p").show();
+  $(".botao2").hide();
+  $(".mensagem2").hide();
+  $(".mensagem1").hide();
+  $(".mensagem3").hide();
+  $(".mensagem4").hide();
+  $(".mensagem5").hide();
+  $(".mensagem6").hide();
+  $(".mensagem7").hide();
+  $(".item_imagem1").hide();
+  $(".item_imagem2").hide();
+  $(".item_imagem3").hide();
+  $(".item_imagem4").hide();
+  $(".item_imagem5").hide();
+  $(".item_imagem6").hide();
+  $(".item_imagem7").hide();
+   click1 = 0;
+   click2 = 0;
+   click3 = 0;
+   click4 = 0;
+   click5 = 0;
+   click6 = 0;
+   click7 = 0;
+   placar = 0;
+});
+      }
+      if(click3==1){
+        placar++;
+    $(".final").find("p").text(placar + '/7');
+      }
+});
+  $(".marcador-4").click(function(){
+    $(".lateral h2, .lateral p").hide();
+  $(".botao, .botao2").hide();
+  $('.item_imagem4').show();
+  $(".mensagem4").show();
+  $(".mensagem2").hide();
+  $(".mensagem3").hide();
+  $(".mensagem1").hide();
+  $(".mensagem5").hide();
+  $(".mensagem6").hide();
+  $(".mensagem7").hide();
+  click4++;
+  if(click1>0 && click2>0  && click3>0 && click4>0  && click5>0 && click6>0 && click7>0){
+        $(".botao2").show();
+        $(".botao2").click(function(){
+  $(".lateral h2, .lateral p").show();
+  $(".botao2").hide();
+  $(".mensagem2").hide();
+  $(".mensagem1").hide();
+  $(".mensagem3").hide();
+  $(".mensagem4").hide();
+  $(".mensagem5").hide();
+  $(".mensagem6").hide();
+  $(".mensagem7").hide();
+  $(".item_imagem1").hide();
+  $(".item_imagem2").hide();
+  $(".item_imagem3").hide();
+  $(".item_imagem4").hide();
+  $(".item_imagem5").hide();
+  $(".item_imagem6").hide();
+  $(".item_imagem7").hide();
+   click1 = 0;
+   click2 = 0;
+   click3 = 0;
+   click4 = 0;
+   click5 = 0;
+   click6 = 0;
+   click7 = 0;
+   placar = 0;
+});
+      }
+      if(click4==1){
+        placar++;
+    $(".final").find("p").text(placar + '/7');
+      }
+});
+  $(".marcador-5").click(function(){
+    $(".lateral h2, .lateral p").hide();
+  $(".botao, .botao2").hide();
+  $('.item_imagem5').show();;
+  $(".mensagem5").show();
+  $(".mensagem2").hide();
+  $(".mensagem3").hide();
+  $(".mensagem4").hide();
+  $(".mensagem1").hide();
+  $(".mensagem6").hide();
+  $(".mensagem7").hide();
+  click5++;
+  if(click1>0 && click2>0  && click3>0 && click4>0  && click5>0 && click6>0 && click7>0){
+        $(".botao2").show();
+        $(".botao2").click(function(){
+  $(".lateral h2, .lateral p").show();
+  $(".botao2").hide();
+  $(".mensagem2").hide();
+  $(".mensagem1").hide();
+  $(".mensagem3").hide();
+  $(".mensagem4").hide();
+  $(".mensagem5").hide();
+  $(".mensagem6").hide();
+  $(".mensagem7").hide();
+  $(".item_imagem1").hide();
+  $(".item_imagem2").hide();
+  $(".item_imagem3").hide();
+  $(".item_imagem4").hide();
+  $(".item_imagem5").hide();
+  $(".item_imagem6").hide();
+  $(".item_imagem7").hide();
+   click1 = 0;
+   click2 = 0;
+   click3 = 0;
+   click4 = 0;
+   click5 = 0;
+   click6 = 0;
+   click7 = 0;
+   placar = 0;
+});
+      }
+      if(click5==1){
+        placar++;
+    $(".final").find("p").text(placar + '/7');
+      }
+});
+  $(".marcador-6").click(function(){
+    $(".lateral h2, .lateral p").hide();
+  $(".botao, .botao2").hide();
+  $('.item_imagem6').show();
+  $(".mensagem6").show();
+  $(".mensagem2").hide();
+  $(".mensagem3").hide();
+  $(".mensagem4").hide();
+  $(".mensagem5").hide();
+  $(".mensagem1").hide();
+  $(".mensagem7").hide();
+  click6++;
+  if(click1>0 && click2>0  && click3>0 && click4>0  && click5>0 && click6>0 && click7>0){
+        $(".botao2").show();
+        $(".botao2").click(function(){
+  $(".lateral h2, .lateral p").show();
+  $(".botao2").hide();
+  $(".mensagem2").hide();
+  $(".mensagem1").hide();
+  $(".mensagem3").hide();
+  $(".mensagem4").hide();
+  $(".mensagem5").hide();
+  $(".mensagem6").hide();
+  $(".mensagem7").hide();
+  $(".item_imagem1").hide();
+  $(".item_imagem2").hide();
+  $(".item_imagem3").hide();
+  $(".item_imagem4").hide();
+  $(".item_imagem5").hide();
+  $(".item_imagem6").hide();
+  $(".item_imagem7").hide();
+   click1 = 0;
+   click2 = 0;
+   click3 = 0;
+   click4 = 0;
+   click5 = 0;
+   click6 = 0;
+   click7 = 0;
+   placar = 0;
+});
+      }
+      if(click6==1){
+        placar++;
+    $(".final").find("p").text(placar + '/7');
+      }
+});
+  
+  $(".marcador-7").click(function(){
+    $(".lateral h2, .lateral p").hide();
+  $(".botao, .botao2").hide();
+  $('.item_imagem7').show();
+  $(".mensagem7").show();
+  $(".mensagem2").hide();
+  $(".mensagem3").hide();
+  $(".mensagem4").hide();
+  $(".mensagem5").hide();
+  $(".mensagem6").hide();
+  $(".mensagem1").hide();
+  click7++;
+  if(click1>0 && click2>0  && click3>0 && click4>0  && click5>0 && click6>0 && click7>0){
+        $(".botao2").show();
+        $(".botao2").click(function(){
+  $(".lateral h2, .lateral p").show();
+  $(".botao2").hide();
+  $(".mensagem2").hide();
+  $(".mensagem1").hide();
+  $(".mensagem3").hide();
+  $(".mensagem4").hide();
+  $(".mensagem5").hide();
+  $(".mensagem6").hide();
+  $(".mensagem7").hide();
+  $(".item_imagem1").hide();
+  $(".item_imagem2").hide();
+  $(".item_imagem3").hide();
+  $(".item_imagem4").hide();
+  $(".item_imagem5").hide();
+  $(".item_imagem6").hide();
+  $(".item_imagem7").hide();
+   click1 = 0;
+   click2 = 0;
+   click3 = 0;
+   click4 = 0;
+   click5 = 0;
+   click6 = 0;
+   click7 = 0;
+   placar = 0;
+});
+      }
+      if(click7==1){
+        placar++;
+    $(".final").find("p").text(placar + '/7');
+      }
+});
+});
+});
